8a5302862cad86740b6f13eb4566af554952aff3
Vidyaranya-Gavai-TTPL <vidyaranya.gavai@tekditechnologies.com>
2024-07-02 12:14:27 +0530

    Initial Staging

---
+++ src/customers/controllers/customers/customers.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Param, Res, Req, ParseIntPipe, HttpException, HttpStatus } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CustomersService } from 'src/customers/services/customers/customers.service';
+
+@Controller('customers')
+export class CustomersController {
+    constructor(private customerService: CustomersService) { }
+    
+    @Get(':id')
+    getCustomer(@Param('id', ParseIntPipe) id: number, @Req() req: Request, @Res() res: Response) {
+        const customer = this.customerService.findCustomerById(id); 
+        if(customer){
+            res.status(200).json(customer);
+        }else{
+            res.status(400).json({msg: "Customer Not Found"})
+        }
+    }
+
+    // NestJS Way
+    @Get('/search/:id')
+    searchCustomer(@Param('id', ParseIntPipe) id: number) {
+        const customer = this.customerService.findCustomerById(id);
+        if(customer) return customer; // note that if you include responce object in the parameters, you have to return the responce through that object
+        else throw new HttpException('Customer Not Found', HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/customers/services/customers/customers.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CustomersService {
+    users = [
+        {
+            id: 1,
+            email: "johndoe@gmail.com",
+            createdAt: new Date()
+        },
+        {
+            id: 2,
+            email: "janesmith@gmail.com",
+            createdAt: new Date()
+        },
+        {
+            id: 3,
+            email: "arthurdayne@gmail.com",
+            createdAt: new Date()
+        },
+        {
+            id: 4,
+            email: "bobcoyle@gmail.com",
+            createdAt: new Date()
+        }
+    ]
+    findCustomerById(id: number){
+        return this.users.find((user)=>user.id===id);
+    }
+}

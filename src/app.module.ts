d497bdfaa1dba88f99f034133c0ca736e3f331d6
Vidyaranya-Gavai-TTPL <vidyaranya.gavai@tekditechnologies.com>
2024-07-04 17:05:11 +0530

    Mysql Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CustomersModule, UsersModule]
+  imports: [CustomersModule, UsersModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'user_vg',
+    password: 'erwinSmith',
+    database: 'nest_practice',
+    entities: [],
+    synchronize: true,
+  })]
 })
 export class AppModule {}
